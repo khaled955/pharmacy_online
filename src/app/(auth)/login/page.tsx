62fbff49b9f5274d0672d78ee9ad5353d326920e
khaled955 <khaledmansour104@yahoo.com>
2026-04-14 15:26:16 +0300

    create build file

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,7 +3,8 @@ import LoginForm from "./_components/login-form";
 export default function LoginPage() {
   return (
     <div>
-        <LoginForm />
+      {/* login-form */}
+      <LoginForm />
     </div>
   );
 }
